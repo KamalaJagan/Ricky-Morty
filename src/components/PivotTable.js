df1ab62a65d936e51c6121b32bc300c7ef6bd179
KamalaJagan <87570607+KamalaJagan@users.noreply.github.com>
2024-02-08 15:13:40 +0530

    first document add

--- src/components/PivotTable.js
+++ src/components/PivotTable.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import PivotTableUI from "react-pivottable/PivotTableUI";
 import "react-pivottable/pivottable.css";
 import Papa from "papaparse";
+import "./pivottable-custom.css";
+import { TableRenderers } from "react-pivottable/TableRenderers";
 
-const PivotTable = () => {
+const PivotTable = (props) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("/mokeData.csv");
       const csvData = await response.text();
+
       const parsedData = Papa.parse(csvData, {
         header: true,
         dynamicTyping: true,
@@ -23,11 +26,13 @@ const PivotTable = () => {
 
   return (
     <div className={PivotTable}>
-      <h2>Pivot Table</h2>
+      <h2 className="">Pivot Table</h2>
       <PivotTableUI
         data={data}
         onChange={(s) => console.log(s)}
         style={{ cursor: "pointer" }}
+        renderers={Object.assign({}, TableRenderers)}
+        {...props}
       />
     </div>
   );
